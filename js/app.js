1c984e215eaa200af559dc6355ef5f3587bbb44a
Ty Frith <tyfrith@gmail.com>
2015-04-23 19:59:20 -0700

    couple of tweaks

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-var app = angular.module('');
+var app = angular.module('readingList', []);  //not sure if I will even need ngRoute... single page is the goal ya?
+
+app.config(function($routeProvider) {
+
+});
